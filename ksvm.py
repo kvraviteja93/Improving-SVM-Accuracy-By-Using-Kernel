dc53aa7354e20d2b03e72b7a32c5d707fde85590
kvraviteja93 <kvraviteja93@gmail.com>
2018-10-13 13:38:46 +0100

    first commit

---
+++ ksvm.py
@@ -0,0 +1,80 @@
+#Importing The Libraries
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+#Importing The Dataset
+dataset=pd.read_csv("Social_Network_Ads.csv")
+
+
+#Separating Independent and Dependent Features
+X=dataset.iloc[:,2:4].values
+y=dataset.iloc[:,-1].values
+
+
+#Splitting The Dataset Into Train and Tet Set
+from sklearn.cross_validation import train_test_split
+X_train,X_test,y_train,y_test=train_test_split(X,y,test_size=0.25,random_state=0)
+
+
+#Scaling The Features
+from sklearn.preprocessing import StandardScaler
+sc_X=StandardScaler()
+X_train=sc_X.fit_transform(X_train)
+X_test=sc_X.fit_transform(X_test)
+
+
+#Fitting The Kernel SVM Model To The Dataset
+from sklearn.svm import SVC
+clf=SVC(kernel="rbf",random_state=0)
+clf.fit(X_train,y_train)
+
+
+#Predicting The Results
+y_pred=clf.predict(X_test)
+
+
+#Checking The Accuracy of The Model
+from sklearn.metrics import accuracy_score,confusion_matrix
+acc=accuracy_score(y_test,y_pred)
+con=confusion_matrix(y_test,y_pred)
+
+
+# Visualising the Training set results
+%matplotlib qt5
+from matplotlib.colors import ListedColormap
+X_set, y_set = X_train, y_train
+X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 1, stop = X_set[:, 0].max() + 1, step = 0.01),
+                     np.arange(start = X_set[:, 1].min() - 1, stop = X_set[:, 1].max() + 1, step = 0.01))
+plt.contourf(X1, X2, clf.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
+             alpha = 0.75, cmap = ListedColormap(('red', 'green')))
+plt.xlim(X1.min(), X1.max())
+plt.ylim(X2.min(), X2.max())
+for i, j in enumerate(np.unique(y_set)):
+    plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1],
+                c = ListedColormap(('red', 'green'))(i), label = j)
+plt.title('Kernel SVM(Training set)')
+plt.xlabel('Age')
+plt.ylabel('Estimated Salary')
+plt.legend()
+plt.show()
+
+
+# Visualising the Test set results
+from matplotlib.colors import ListedColormap
+X_set, y_set = X_test, y_test
+X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 1, stop = X_set[:, 0].max() + 1, step = 0.01),
+                     np.arange(start = X_set[:, 1].min() - 1, stop = X_set[:, 1].max() + 1, step = 0.01))
+plt.contourf(X1, X2, clf.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
+             alpha = 0.75, cmap = ListedColormap(('red', 'green')))
+plt.xlim(X1.min(), X1.max())
+plt.ylim(X2.min(), X2.max())
+for i, j in enumerate(np.unique(y_set)):
+    plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1],
+                c = ListedColormap(('red', 'green'))(i), label = j)
+plt.title('Kernel SVM(Test set)')
+plt.xlabel('Age')
+plt.ylabel('Estimated Salary')
+plt.legend()
+plt.show()
